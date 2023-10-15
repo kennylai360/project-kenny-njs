67a945c91ec63ef492b8746888741dff31e7f6c7
Kenny Lai <kennylai360@gmail.com>
2023-10-15 17:30:41 +0100

    Some update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ const font = Bitter({
   subsets: ['latin'],
 });
 
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
+
 export default function RootLayout({
   children,
 }: {
